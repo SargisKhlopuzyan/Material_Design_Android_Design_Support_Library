59bb9ad679587f80310289b643896c8fa325ea0c
Sargis Khlopuzyan <sargiskh@SARGISKH-394>
2018-01-23 11:44:12 +0400

    Android Design Library

---
+++ app/src/main/java/com/example/sargiskh/material_design_android_design_support_library/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sargiskh.material_design_android_design_support_library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
